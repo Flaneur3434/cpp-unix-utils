0aeef7e35b553323c139360a414f84c4b1da076f
GrapeJuiceSoda <kendallcarmel@gmail.com>
2023-03-05 22:36:59 -0800

    debugged and refactored argument parsing

--- cksum/cksum.cpp
+++ cksum/cksum.cpp
@@ -1,78 +1,94 @@
 #include <algorithm>
+#include <optional>
 #include <string>
+#include <string_view>
+#include <type_traits>
 #include <variant>
 #include <vector>
 #include <unordered_map>
 #include <iostream>
 #include <charconv>
-#include <optional>
+#include <utility>
 
 class CmdLineParser{
 public:
-	using Arg = std::variant<std::string, int>;
-
 	explicit CmdLineParser (int& argc, char *argv[]){
 		CmdOptionsFromStdin(argc, argv);
 	}
+	~CmdLineParser();
 
+	using Arg = std::variant<std::string, int>;
 	void CmdOptionsFromStdin(int& argc, char *argv[]);
-	Arg TryParseString(std::string_view& arg);
-	std::optional<Arg> getCmdOptions(std::string&& getStr);
+	Arg TryParseString(std::string_view&& arg);
+	std::optional<Arg> getCmdOptions(const std::string& getStr);
 
-
-private:
 	std::unordered_map<std::string, Arg> mParsedArgs;
 };
 
-void CmdLineParser::CmdOptionsFromStdin(int& argc, char **argv) {
-    for (int i = 1; i < argc; i++) {
-        std::string_view cmd = std::string_view(argv[i]);
-		size_t pos = cmd.find('-');
-		
-		if (pos == std::string_view::npos) {
-			std::cout << "Wrong command line argument" << std::endl;
-		}
-
-		cmd = cmd.substr(pos + 1);
-		mParsedArgs[std::string(cmd.data(), cmd.size())] = TryParseString(cmd);
-    }
+CmdLineParser::~CmdLineParser() {
+	mParsedArgs.~unordered_map();
 }
 
-CmdLineParser::Arg CmdLineParser::TryParseString(std::string_view& arg) {
+void CmdLineParser::CmdOptionsFromStdin(int& argc, char **argv) {
+	for (int i = 1; i < argc; i++) {
+		std::string_view cmd = std::string_view(argv[i]);
+		size_t pos = cmd.find('-');
+		if (pos != std::string_view::npos) {
+			// arguments with `-` are flags
+			cmd = cmd.substr(pos + 1);
+			mParsedArgs[std::string(cmd.data(), cmd.size())] = TryParseString(std::string_view(argv[++i]));
+		} else {
+			// else argument is assumed to be file name
+			mParsedArgs["filename"] = TryParseString(std::move(cmd));
+		}
+	}
+}
+
+CmdLineParser::Arg CmdLineParser::TryParseString(std::string_view&& arg) {
 	int result{};
 	const auto [ptr, ec] = std::from_chars(arg.data(), arg.data() + arg.size(), result);
 	if (ec == std::errc::invalid_argument || ec == std::errc::result_out_of_range) {
-		std::cerr << "Error parsing interger: " << arg << std::endl;
 		return std::string(arg.data(), arg.size());
 	}
 
 	return result;
 }
 
-std::optional<CmdLineParser::Arg> CmdLineParser::getCmdOptions(std::string&& getStr) {
+std::optional<CmdLineParser::Arg> CmdLineParser::getCmdOptions(const std::string& getStr) {
 	auto cmd = mParsedArgs.find(getStr);
 	if (cmd == mParsedArgs.end()) {
 		return {};
 	}
 
-	return cmd->first;
+	return cmd->second;
 }
 
 int main(int argc, char *argv[]){
 	auto clp = CmdLineParser(argc, argv);
-	auto get = clp.getCmdOptions(std::string("1"));
-	if (!get) {
-		std::cout << "did not find matching argument" << std::endl;
+	auto fn = clp.getCmdOptions("filename");
+	if (!fn) {
+		std::cerr << "No file is given: aborting process!" << std::endl;
 		return -1;
 	}
 
-	std::visit([](auto&& arg) {
-		using T = std::decay_t<decltype(arg)>;
-		if constexpr (std::is_same_v<T, std::string>) {
-			std::cout << "Found argument: " << arg << std::endl;
-		}
-		else if constexpr (std::is_same_v<T, int>) {
-			std::cout << "Found argument: " << arg << std::endl;
-		}
-	}, *get);
+	auto algo_ver = clp.getCmdOptions("o");
+	if (!algo_ver) {
+		// TODO: default algorithm is choosen
+		return 0;
+	}
+
+	std::cout << "using historic algorithms instead of the superior default one." << std::endl;
+	switch(std::get<int>(*algo_ver)) {
+	case 1:
+		puts("This is a 16-bit checksum, with a right rotation before each addition");
+		break;
+	case 2:
+		puts("This is a 32-bit check‐sum.");
+		break;
+	case 3:
+		puts("Algorithm 3 is what is commonly called the ‘32bit CRC’ algorithm.");
+		break;
+	default:
+		std::cerr << "illegal argument to -o option" << std::endl;
+	}
 }
